f61236f09fed6f76c7ba01f0e119cf645390520e
TVI-BIZ <vladebay1@gmail.com>
2019-06-08 15:22:23 +0200

    UIContainerView

---
+++ UIKit/UIContainerView/UIContainerView/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UIContainerView
+//
+//  Created by Vlad Tagunkov on 08/06/2019.
+//  Copyright © 2019 TVI Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageCover: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
