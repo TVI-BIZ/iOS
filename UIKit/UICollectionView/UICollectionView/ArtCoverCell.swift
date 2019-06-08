3e9ffa7a92833d40ec071d24ce553c736e7a69ef
TVI-BIZ <vladebay1@gmail.com>
2019-06-08 13:10:56 +0200

    UICollectionView

---
+++ UIKit/UICollectionView/UICollectionView/ArtCoverCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArtCoverCell.swift
+//  UICollectionView
+//
+//  Created by Vlad Tagunkov on 07/06/2019.
+//  Copyright © 2019 TVI Software. All rights reserved.
+//
+
+import UIKit
+
+class ArtCoverCell: UICollectionViewCell {
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+}
