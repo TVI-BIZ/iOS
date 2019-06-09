bdce9af753f2b625d39cea2dab903526976bb8bf
TVI-BIZ <vladebay1@gmail.com>
2019-06-09 23:48:39 +0200

    4 - Add place done change data model

--- BestPlaces/BestPlaces/NewPlaceViewController.swift
+++ BestPlaces/BestPlaces/NewPlaceViewController.swift
@@ -10,22 +10,48 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
 
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var placeImage: UIImageView!
+    @IBOutlet weak var placeName: UITextField!
+    
+    @IBOutlet weak var placeType: UITextField!
+    @IBOutlet weak var placeLocation: UITextField!
+    
+    var newPlace: Place?
+    var imageIsChanged = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView()
+        
+        saveButton.isEnabled = false
+        
+        placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        
         
     }
     // MARK: Table view delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let photoIcon = #imageLiteral(resourceName: "photo")
+            
+            
             let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             
             let camera = UIAlertAction(title: "Camera", style: .default) { _ in
                 self.chooseImagePicker(sourse: .camera)
             }
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
             let photo = UIAlertAction(title: "Photo", style: .default) { _ in
                 self.chooseImagePicker(sourse: .photoLibrary)
             }
+            
+            photo.setValue(photoIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
             
             actionSheet.addAction(camera)
@@ -39,8 +65,22 @@ class NewPlaceViewController: UITableViewController {
             view.endEditing(true)
         }
     }
-    
+    func saveNewPlace(){
+        var image: UIImage?
+        
+        if imageIsChanged{
+            image = placeImage.image
+        } else {
+            image = #imageLiteral(resourceName: "imagePlaceholder")
+        }
+        
+        newPlace = Place(name: placeName.text!, location: placeLocation.text, type: placeType.text, restaurantImage: nil, image: image)
+    }
 
+    @IBAction func cancellAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+    }
 }
 // MARK: Text field delegate
 extension NewPlaceViewController: UITextFieldDelegate {
@@ -50,17 +90,37 @@ extension NewPlaceViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
-    
+    @objc private func textFieldChanged(){
+        if placeName.text?.isEmpty == false {
+            saveButton.isEnabled = true
+        } else {
+            saveButton.isEnabled = false
+        }
+    }
 }
 // MARK:Work with image
-extension NewPlaceViewController{
+extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func chooseImagePicker(sourse: UIImagePickerController.SourceType){
         if UIImagePickerController.isSourceTypeAvailable(sourse){
+            
+            
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate  = self
             imagePicker.allowsEditing = true
             imagePicker.sourceType = sourse
             present(imagePicker, animated: true, completion: nil)
             
         }
     }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        placeImage.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        placeImage.contentMode = .scaleAspectFill
+        placeImage.clipsToBounds = true
+        
+        imageIsChanged = true
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
--- BestPlaces/BestPlaces/PlaceModel.swift
+++ BestPlaces/BestPlaces/PlaceModel.swift
@@ -6,14 +6,15 @@
 //  Copyright © 2019 TVI Software. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct Place {
     
     var name: String
-    var location: String
-    var type: String
-    var image: String
+    var location: String?
+    var type: String?
+    var restaurantImage: String?
+    var image: UIImage?
     
       static  let restaurantNames = [
             "Burger Heroes", "Kitchen", "Bonsai", "EastFood",
@@ -25,7 +26,7 @@ struct Place {
         
         var places = [Place]()
         for place in restaurantNames{
-            places.append(Place(name: place, location: "Buffalo", type: "Restaurant", image: place))
+            places.append(Place(name: place, location: "Buffalo", type: "Restaurant", restaurantImage: place, image:nil))
         }
         
         
