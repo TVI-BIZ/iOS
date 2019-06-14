062c269dd996ffa6455c321eda1a346d3c6b7ca4
TVI-BIZ <vladebay1@gmail.com>
2019-06-14 16:09:43 +0200

    1 - Initila app - starting point. All elements done

---
+++ API/Weather_App_API/Weather_App_API/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_App_API
+//
+//  Created by Vlad Tagunkov on 14/06/2019.
+//  Copyright © 2019 TVI Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
